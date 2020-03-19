09fe891da7e9984d26941a1560ea9412e940c77b
aayush.jain <aayushvjain@gofynd.com>
2020-03-19 14:31:50 +0530

    nitrozen badge added

--- dist/nitrozen.umd.js
+++ dist/nitrozen.umd.js
@@ -117,6 +117,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ }),
 
+/***/ "15f6":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
 /***/ "1c13":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -242,6 +249,17 @@ module.exports = svgs;
 /***/ (function(module, exports, __webpack_require__) {
 
 // extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "493f":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_10_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_10_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_10_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_10_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NBadge_vue_vue_type_style_index_0_lang_less___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("15f6");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_10_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_10_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_10_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_10_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NBadge_vue_vue_type_style_index_0_lang_less___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_10_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_10_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_10_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_10_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NBadge_vue_vue_type_style_index_0_lang_less___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_10_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_10_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_10_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_10_oneOf_1_3_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_NBadge_vue_vue_type_style_index_0_lang_less___WEBPACK_IMPORTED_MODULE_0___default.a); 
 
 /***/ }),
 
@@ -2012,7 +2030,110 @@ var NChips_component = normalizeComponent(
 // CONCATENATED MODULE: ./src/components/NChips/index.js
 
 /* harmony default export */ var components_NChips = (NChips);
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"046ba296-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/NBadge/NBadge.vue?vue&type=template&id=5bec96b6&
+var NBadgevue_type_template_id_5bec96b6_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('transition',{attrs:{"name":"nitrozen-badge"}},[_c('div',{staticClass:"nitrozen-badge",class:[_vm.addClass]},[_vm._v(_vm._s(_vm.title.toUpperCase()))])])}
+var NBadgevue_type_template_id_5bec96b6_staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/NBadge/NBadge.vue?vue&type=template&id=5bec96b6&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/NBadge/NBadge.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var NBadgevue_type_script_lang_js_ = ({
+  name: "nitrozen-badge",
+  props: {
+    state: {
+      type: String,
+      default: "none"
+    },
+    title: {
+      type: String,
+      default: "none"
+    },
+    fill: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    addClass() {
+      let className = "";
+
+      switch (this.state) {
+        case "default":
+        case "none":
+          className = "nitrozen-badge-default";
+          break;
+
+        case "info":
+          className = "nitrozen-badge-info";
+          break;
+
+        case "success":
+          className = "nitrozen-badge-success";
+          break;
+
+        case "warn":
+          className = "nitrozen-badge-warn";
+          break;
+
+        case "error":
+          className = "nitrozen-badge-error";
+          break;
+
+        case "disable":
+          className = "nitrozen-badge-disable";
+          break;
+
+        default:
+          break;
+      }
+
+      if (this.fill) {
+        className += "-fill";
+      }
+
+      return className;
+    }
+
+  }
+});
+// CONCATENATED MODULE: ./src/components/NBadge/NBadge.vue?vue&type=script&lang=js&
+ /* harmony default export */ var NBadge_NBadgevue_type_script_lang_js_ = (NBadgevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./src/components/NBadge/NBadge.vue?vue&type=style&index=0&lang=less&
+var NBadgevue_type_style_index_0_lang_less_ = __webpack_require__("493f");
+
+// CONCATENATED MODULE: ./src/components/NBadge/NBadge.vue
+
+
+
+
+
+
+/* normalize component */
+
+var NBadge_component = normalizeComponent(
+  NBadge_NBadgevue_type_script_lang_js_,
+  NBadgevue_type_template_id_5bec96b6_render,
+  NBadgevue_type_template_id_5bec96b6_staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var NBadge = (NBadge_component.exports);
+// CONCATENATED MODULE: ./src/components/NBadge/index.js
+
+/* harmony default export */ var components_NBadge = (NBadge);
 // CONCATENATED MODULE: ./src/components/index.js
+
 
 
 
@@ -2062,6 +2183,7 @@ var NChips_component = normalizeComponent(
 /* concated harmony reexport NitrozenAutocomplete */__webpack_require__.d(__webpack_exports__, "NitrozenAutocomplete", function() { return components_NAutocomplete; });
 /* concated harmony reexport NitrozenChips */__webpack_require__.d(__webpack_exports__, "NitrozenChips", function() { return components_NChips; });
 /* concated harmony reexport NitrozenInline */__webpack_require__.d(__webpack_exports__, "NitrozenInline", function() { return components_NInline; });
+/* concated harmony reexport NitrozenBadge */__webpack_require__.d(__webpack_exports__, "NitrozenBadge", function() { return components_NBadge; });
 /* concated harmony reexport strokeBtn */__webpack_require__.d(__webpack_exports__, "strokeBtn", function() { return NStrokeBtn; });
 /* concated harmony reexport flatBtn */__webpack_require__.d(__webpack_exports__, "flatBtn", function() { return NFlatBtn; });
 
