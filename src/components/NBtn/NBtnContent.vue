d5e63436c519e0fc0a9131dcc4184c7d50251051
Akash Mane <m.akash2209@gmail.com>
2020-05-20 15:17:52 +0530

    fix loader gif

--- src/components/NBtn/NBtnContent.vue
+++ src/components/NBtn/NBtnContent.vue
@@ -3,12 +3,10 @@
     <div class="social-icon" v-if="icon">
         <img v-if="icon == 'facebook'" src="./../../assets/facebook-icon.png">
         <!-- <img v-if="icon == 'bag'" src="./../../assets/Bag_Icon.svg"> -->
-        
-        
     </div>
     <slot v-if="!showProgress" />
     <div v-if="showProgress" class="n-btn-spin">
-        <img class="n-btn-spinner" style="width: 50px;" src="./../../assets/loader-white.gif">
+        <img class="n-btn-spinner" style="width: 50px;" src="https://raw.githubusercontent.com/gofynd/nitrozen-vue/master/src/assets/loader-white.gif">
     </div>
 </div>
 </template>
