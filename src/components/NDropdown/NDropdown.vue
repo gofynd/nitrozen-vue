0bde80a13b4fa93fb1758876bd3e8f9371caf9e8
abhaygawadefynd <abhaygawade@gofynd.com>
2020-06-02 13:56:49 +0530

    custom options support in dropdown

--- src/components/NDropdown/NDropdown.vue
+++ src/components/NDropdown/NDropdown.vue
@@ -50,33 +50,37 @@
             }"
             @click="selectItem(index, item)"
           >
-            <template v-if="multiple && !item.isGroupLabel">
-              <nitrozen-checkbox
-                :checkboxValue="item.value"
-                @change="setCheckedItem"
-                v-model="selectedItems"
-                :ref="`multicheckbox-${index}`"
-              >
-                <span
-                  :class="{
+            <slot :item="item" :selected="item == selected" name="option">
+              <div class="nitrozen-option-container">
+                <template v-if="multiple && !item.isGroupLabel">
+                  <nitrozen-checkbox
+                    :checkboxValue="item.value"
+                    @change="setCheckedItem"
+                    v-model="selectedItems"
+                    :ref="`multicheckbox-${index}`"
+                  >
+                    <span
+                      :class="{
                     'nitrozen-dropdown-multicheckbox-selected': selectedItems.includes(
                       item.value
                     )
                   }"
-                >{{ item.text }}</span>
-              </nitrozen-checkbox>
-            </template>
-            <template v-else>
-              <span
-                :class="{
+                    >{{ item.text }}</span>
+                  </nitrozen-checkbox>
+                </template>
+                <template v-else>
+                  <span
+                    :class="{
                   'nitrozen-option-child-label':
                     items.find(i => i.isGroupLabel) && !item.isGroupLabel
                 }"
-              >
-                <img v-if="item.logo" class="nitrozen-option-logo" :src="item.logo" alt="logo" />
-                {{ item.text }}
-              </span>
-            </template>
+                  >
+                    <img v-if="item.logo" class="nitrozen-option-logo" :src="item.logo" alt="logo" />
+                    {{ item.text }}
+                  </span>
+                </template>
+              </div>
+            </slot>
           </span>
           <span v-if="searchable && items.length == 0" class="nitrozen-option">No {{ label }} Found</span>
         </div>
@@ -121,7 +125,14 @@ export default {
       }
     },
     /**
-     * disbaled dropdown
+     * custom dropdown
+     */
+    custom: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * disabled dropdown
      */
     disabled: {
       type: Boolean,
