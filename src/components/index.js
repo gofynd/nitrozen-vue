46cc7a592f1600d03dbfbe7046cd03b03a57332b
Khushnood <khushnoodhussain@fynd-external.com>
2023-03-30 15:53:53 +0530

    fixed nudge transitions

--- src/components/index.js
+++ src/components/index.js
@@ -18,6 +18,7 @@ import NitrozenStepper from './NStepper';
 import NitrozenToggleBtn from './NToggleBtn';
 import NitrozenTooltip from './NTooltip';
 import NitrozenCustomForm from './NCustomForm';
+import NitrozenNudge from './NNudge';
 
 export {
     NitrozenAutocomplete,
@@ -40,4 +41,5 @@ export {
     NitrozenToggleBtn,
     NitrozenTooltip,
     NitrozenCustomForm,
+    NitrozenNudge
 }
