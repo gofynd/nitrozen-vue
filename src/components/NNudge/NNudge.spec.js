ef8795254c7185711ed2304557372423f88bea9f
Khushnood <khushnoodhussain@fynd-external.com>
2023-03-31 10:03:16 +0530

    fixed testing file

--- src/components/NNudge/NNudge.spec.js
+++ src/components/NNudge/NNudge.spec.js
@@ -1,12 +1,8 @@
 import { mount } from '@vue/test-utils';
 import NNudge from './NNudge.vue';
-import NUuid from '../../utils/NUuid';
 
 describe('Nudge Component', () => {
-  it('should render simple radio with all clasess', async () => {
-    const id = NUuid();
-    const change = jest.fn();
-    const _input = jest.fn();
+  it('should render simple nudge with all clasess', async () => {
 
     const wrapper = mount(NNudge, {
       propsData: {
@@ -20,7 +16,6 @@ describe('Nudge Component', () => {
 
     wrapper.vm.open()
     expect(wrapper.vm.nudgeList.length > 0).toBe(true)
-    // expect(wrapper.classes('active')).toBe(true)
   });
 
 })
