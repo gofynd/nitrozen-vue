e7806c9a4dad61b18a87525643225cbe65db92d6
venkatesh7.s <venkatesh7.s@ril.com>
2023-07-27 16:25:38 +0530

    bottomsheet css updated

--- src/components/NBottomsheet/NBottomsheet.vue
+++ src/components/NBottomsheet/NBottomsheet.vue
@@ -1,8 +1,9 @@
 <template>
-    <transition name="bottom-sheet">
+    
       <div v-if="show" class="bottom-sheet-overlay" @click="onClose">
+        <transition name="bottom-sheet">
         <div class="bottom-sheet-content" @click.stop>
-            <button class="bottom-sheet-close" @click="onClose"><nitrozen-icon  name="close" ></nitrozen-icon></button>
+            <button class="bottom-sheet-close" @click="onClose"><nitrozen-icon  name="close" size="24"></nitrozen-icon></button>
           <div class="bottom-sheet-header">
             <h3 v-if="title">{{ title }}</h3>
           <p v-if="description">{{ description }}</p>
@@ -11,8 +12,9 @@
             <slot></slot>
           </div>
         </div>
-      </div>
     </transition>
+
+      </div>
   </template>
   
   <script>
@@ -44,68 +46,143 @@
   </script>
   
   <style>
-  .bottom-sheet-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
-    z-index: 9999; 
-  }
-  
-  .bottom-sheet-content {
-    background-color: #fff;
-    border-radius: 24px 24px 0 0;
-    padding: 60px 24px 24px 24px;
-    width: 100%;
-    position: absolute;
-    max-width: 360px;
-    right: 0;
-    bottom: 0;
-    margin-right: 60px;
+.bottom-sheet-overlay {
 
-    @media only screen and (max-width: 767px)  {
-      max-width:100%;
-      width:100%;
-      margin-right: 0px;
-    }
-  }
-  
-  .bottom-sheet-header {
-    text-align: center;
-    margin-bottom: 10px;
-  }
-  
-  .bottom-sheet-inner-content {
-    margin-bottom: 20px;
-  }
-  
-  .bottom-sheet-actions {
-    display: flex;
-    justify-content: flex-end;
-  }
-  
-  .bottom-sheet-enter-active, .bottom-sheet-leave-active {
-    transition: opacity 0.3s, transform 0.3s;
-  }
-  
-  .bottom-sheet-enter, .bottom-sheet-leave-to {
-    opacity: 0;
-    transform: translateY(100%);
-  }
+position: fixed;
 
-  .bottom-sheet-close{
-    position: absolute;
-    right: 0;
-    top: 0;
-    margin-right:24px;
-    margin-top: 24px;
-    border: none;
-    background: transparent;
-  }
+top: 0;
+
+left: 0;
+
+right: 0;
+
+bottom: 0;
+
+background-color: rgba(0, 0, 0, 0.5);
+
+display: flex;
+
+justify-content: center;
+
+align-items: flex-end;
+
+z-index: 9999;
+
+}
+.bottom-sheet-content {
+
+background-color: #fff;
+
+border-radius: 24px 24px 0 0;
+
+padding: 40px 24px 24px 24px;
+
+width: 100%;
+
+position: absolute;
+
+max-width: 360px;
+
+right: 0;
+
+bottom: 0;
+
+margin-right: 60px;
+
+@media only screen and (max-width: 767px)  {
+
+  max-width:100%;
+
+  width:100%;
+
+  margin-right: 0px;
+
+}
+
+}
+
+.bottom-sheet-header {
+
+text-align: left;
+
+margin-bottom: 10px;
+
+}
+
+
+
+.bottom-sheet-inner-content {
+
+margin-bottom: 20px;
+
+}
+
+
+
+.bottom-sheet-actions {
+
+display: flex;
+
+justify-content: flex-end;
+
+}
+
+
+
+.bottom-sheet-enter-active, .bottom-sheet-leave-active {
+
+transition: opacity 0.3s, transform 0.3s;
+
+}
+
+
+
+.bottom-sheet-enter, .bottom-sheet-leave-to {
+
+opacity: 0;
+
+transform: translateY(100%);
+
+}
+
+
+
+
+.bottom-sheet-close{
+
+position: absolute;
+
+right: 0;
+
+top: 0;
+
+height: 48px;
+
+margin-top: 24px;
+
+margin-right: 16px;
+
+padding: 12px;
+
+border: none;
+
+background: transparent;
+
+}
+
+
+
+
+.bottom-sheet-close:hover{
+
+border-radius: 24px;
+
+background-color: #e7ebf8;
+
+cursor: pointer;
+
+padding: 12px;
+
+}
   </style>
   
--- src/components/NHeading/NHeading.vue
+++ src/components/NHeading/NHeading.vue
@@ -6,6 +6,7 @@
 
   
 <script>
+
 export default {
   name: "nitrozen-heading",
   props: {
