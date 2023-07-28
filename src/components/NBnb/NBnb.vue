51cbcfa2ba037aa49023a5798a46738c3f4d1c98
venkatesh7.s <venkatesh7.s@ril.com>
2023-07-28 16:59:41 +0530

    bottom nvaigation bnb css updated

--- src/components/NBnb/NBnb.vue
+++ src/components/NBnb/NBnb.vue
@@ -1,63 +1,85 @@
 <template>
     <nav class="bottom-nav">
-      <a
-        v-for="(item, index) in items"
-        :key="index"
-        :href="item.href"
-        :aria-label="item['aria-label']"
-        @click="handleItemClick(item)"
-      >
-        <div class="icon-wrapper">
-          <div v-html="item.icon"></div>
-        </div>
-        <div class="title">{{ item.title }}</div>
-      </a>
+        <a v-for="(item, index) in items" :key="index" :href="item.href" :aria-label="item['aria-label']"
+            @click="handleItemClick(item)">
+            <div class="icon-wrapper">
+                <div v-html="item.icon"></div>
+            </div>
+            <div v-if="item.title" class="title">{{ item.title }}</div>
+        </a>
     </nav>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     name: 'BottomNav',
     props: {
-      items: {
-        type: Array,
-        required: true,
-      },
+        items: {
+            type: Array,
+            required: true,
+        },
     },
     methods: {
-      handleItemClick(item) {
-        if (!item.disabled && item.onClick) {
-          item.onClick();
-        }
-      },
+        handleItemClick(item) {
+            if (!item.disabled && item.onClick) {
+                item.onClick();
+            }
+        },
     },
-  };
-  </script>
+};
+</script>
   
-  <style>
-  .bottom-nav {
+<style lang="less">
+@import '../../../src/base/variable.less';
+
+.bottom-nav {
+
     display: flex;
+
     justify-content: space-around;
-    background-color: #f0f0f0;
-    padding: 10px;
-  }
-  
-  .bottom-nav a {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-decoration: none;
-    color: #333;
-  }
-  
-  .bottom-nav a .icon-wrapper {
-    width: 24px;
-    height: 24px;
-  }
-  
-  .bottom-nav a .title {
-    font-size: 12px;
-    margin-top: 4px;
-  }
-  </style>
+
+    background-color: @GreyColor;
+
+    padding: 10px 0px;
+
+    position: fixed;
+
+    bottom: 0;
+
+    width: 100%;
+
+    z-index: 1;
+
+    left: 0;
+
+    box-shadow: 0 4px 16px @Shadow;
+    a {
+
+        display: flex;
+
+        flex-direction: column;
+
+        align-items: center;
+
+        text-decoration: none;
+
+        color: @TypographySecondaryColor;
+        .icon-wrapper {
+
+            width: 24px;
+
+            height: 24px;
+
+        }
+        .title {
+
+            font-size: 12px;
+
+            margin-top: 4px;
+
+        }
+
+    }
+}
+</style>
   
