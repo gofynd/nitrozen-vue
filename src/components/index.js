518c2adc91f75874fc5ba3190b53411be1166eff
venkatesh7.s <venkatesh7.s@ril.com>
2023-08-07 21:23:31 +0530

    jds comp new request

--- src/components/index.js
+++ src/components/index.js
@@ -28,6 +28,17 @@ import NitrozenBreadcrumbs from './NBreadcrumbs';
 import NitrozenImage from './NImage'
 import NitrozenLink from './NLink'
 import NitrozenStepitem from './NStepitem'
+import NitrozenHeading from './NHeading';
+import NitrozenText from './NText';
+import NitrozenGrid from './NGrid';
+import NitrozenFlexBox from './NFlexBox';
+import NitrozenListblock from './NListblock';
+import NitrozenDivider from './NDivider';
+import NitrozenToaster from './NToaster';
+import NitrozenSidepanel from './NSidepanel';
+import NitrozenNudge from './NNudge';
+import NitrozenBottomsheet from './NBottomsheet';
+import NitrozenBnb from './NBnb';
 
 export {
     NitrozenAutocomplete,
@@ -59,5 +70,16 @@ export {
     NitrozenBreadcrumbs,
     NitrozenImage,
     NitrozenLink,
-    NitrozenStepitem
+    NitrozenStepitem,
+    NitrozenHeading,
+    NitrozenText,
+    NitrozenGrid,
+    NitrozenFlexBox,
+    NitrozenListblock,
+    NitrozenDivider,
+    NitrozenToaster,
+    NitrozenSidepanel,
+    NitrozenNudge,
+    NitrozenBottomsheet,
+    NitrozenBnb
 }
