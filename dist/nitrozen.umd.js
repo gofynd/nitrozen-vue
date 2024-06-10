5b4f4549960720fd60b7651b82ef42a5e7d6c47c
ekatakumari <ekatakumari@gofynd.com>
2024-06-10 14:13:31 +0530

    ID:FPCO-34211;DONE:100;HOURS:1;

--- dist/nitrozen.umd.js
+++ dist/nitrozen.umd.js
@@ -96,7 +96,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 /***/ }),
 
-/***/ 7183:
+/***/ 9806:
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -142,8 +142,8 @@ var staticRenderFns = [];
 // EXTERNAL MODULE: ./node_modules/json-logic-js/logic.js
 var logic = __webpack_require__(962);
 var logic_default = /*#__PURE__*/__webpack_require__.n(logic);
-;// CONCATENATED MODULE: ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/NCustomForm/NCustomFormInput.vue?vue&type=template&id=475d1896&scoped=true&
-var NCustomFormInputvue_type_template_id_475d1896_scoped_true_render = function render() {
+;// CONCATENATED MODULE: ./node_modules/babel-loader/lib/index.js??clonedRuleSet-82.use[1]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/NCustomForm/NCustomFormInput.vue?vue&type=template&id=2605ae10&scoped=true&
+var NCustomFormInputvue_type_template_id_2605ae10_scoped_true_render = function render() {
   var _vm = this,
     _c = _vm._self._c;
   return _c('div', {
@@ -362,7 +362,7 @@ var NCustomFormInputvue_type_template_id_475d1896_scoped_true_render = function 
     }
   }, [_vm._v(" Add ")])], 2)] : _vm._e(), _vm.errorMessage ? _c('nitrozen-error', [_vm._v(_vm._s(_vm.errorMessage))]) : _vm._e()], 2);
 };
-var NCustomFormInputvue_type_template_id_475d1896_scoped_true_staticRenderFns = [];
+var NCustomFormInputvue_type_template_id_2605ae10_scoped_true_staticRenderFns = [];
 
 ;// CONCATENATED MODULE: ./src/components/NCustomForm/InputTypes.js
 let InputTypes = {
@@ -735,7 +735,7 @@ var NInline = __webpack_require__(2246);
     NitrozenButton: NBtn/* default */.Z,
     NitrozenInline: NInline/* default */.Z,
     VueTelInput: (vue_tel_input_default()),
-    NitrozenCustomForm: () => Promise.resolve(/* import() */).then(__webpack_require__.bind(__webpack_require__, 7183)) // Loophole for circular imports issue
+    NitrozenCustomForm: () => Promise.resolve(/* import() */).then(__webpack_require__.bind(__webpack_require__, 9806)) // Loophole for circular imports issue
   },
 
   event: "change",
@@ -755,9 +755,14 @@ var NInline = __webpack_require__(2246);
     countryCodeChange(value) {
       this.formInputValue.code = value.dialCode;
     },
-    inputChanged() {
+    inputChanged(event) {
       this.errorMessage = null;
-      this.$emit("change", this.formInputValue);
+      const value = event.target.value;
+      if (value === '+' || value === '-') {
+        this.inputValue = '';
+      } else {
+        this.$emit("change", this.formInputValue);
+      }
     },
     addResponse() {
       this.formInputValue.push(defaultResponseForInput(this.input.input));
@@ -797,10 +802,10 @@ var NInline = __webpack_require__(2246);
 });
 ;// CONCATENATED MODULE: ./src/components/NCustomForm/NCustomFormInput.vue?vue&type=script&lang=js&
  /* harmony default export */ var NCustomForm_NCustomFormInputvue_type_script_lang_js_ = (NCustomFormInputvue_type_script_lang_js_); 
-;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-74.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-74.use[1]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-74.use[2]!./node_modules/less-loader/dist/cjs.js??clonedRuleSet-74.use[3]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/NCustomForm/NCustomFormInput.vue?vue&type=style&index=0&id=475d1896&prod&lang=less&scoped=true&
+;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-74.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-74.use[1]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-74.use[2]!./node_modules/less-loader/dist/cjs.js??clonedRuleSet-74.use[3]!./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/NCustomForm/NCustomFormInput.vue?vue&type=style&index=0&id=2605ae10&prod&lang=less&scoped=true&
 // extracted by mini-css-extract-plugin
 
-;// CONCATENATED MODULE: ./src/components/NCustomForm/NCustomFormInput.vue?vue&type=style&index=0&id=475d1896&prod&lang=less&scoped=true&
+;// CONCATENATED MODULE: ./src/components/NCustomForm/NCustomFormInput.vue?vue&type=style&index=0&id=2605ae10&prod&lang=less&scoped=true&
 
 // EXTERNAL MODULE: ./node_modules/@vue/cli-service/node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js
 var componentNormalizer = __webpack_require__(3736);
@@ -815,11 +820,11 @@ var componentNormalizer = __webpack_require__(3736);
 
 var component = (0,componentNormalizer/* default */.Z)(
   NCustomForm_NCustomFormInputvue_type_script_lang_js_,
-  NCustomFormInputvue_type_template_id_475d1896_scoped_true_render,
-  NCustomFormInputvue_type_template_id_475d1896_scoped_true_staticRenderFns,
+  NCustomFormInputvue_type_template_id_2605ae10_scoped_true_render,
+  NCustomFormInputvue_type_template_id_2605ae10_scoped_true_staticRenderFns,
   false,
   null,
-  "475d1896",
+  "2605ae10",
   null
   
 )
@@ -14336,7 +14341,7 @@ var NStepper_component = (0,componentNormalizer/* default */.Z)(
 // EXTERNAL MODULE: ./src/components/NToggleBtn/index.js + 6 modules
 var NToggleBtn = __webpack_require__(6515);
 // EXTERNAL MODULE: ./src/components/NCustomForm/NCustomForm.vue + 13 modules
-var NCustomForm = __webpack_require__(7183);
+var NCustomForm = __webpack_require__(9806);
 ;// CONCATENATED MODULE: ./src/components/NCustomForm/index.js
 
 /* harmony default export */ var components_NCustomForm = (NCustomForm["default"]);
