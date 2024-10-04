60300e39b384c1d054154d1a2b5b086107d8ea44
Paul Lobo 😎 <paullobo@gofynd.com>
2024-10-04 15:09:08 +0530

    fix pipeline publish-npm node ver

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
+// const path = require('path');
 
-console.log('vue.config.js is being used!'); // Debug log
+// console.log('vue.config.js is being used!'); // Debug log
 
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    }
-  },
-  css: {
-    loaderOptions: {
-      less: {
-        lessOptions: {
-          strictMath: true,
-          noIeCompat: true,
-        },
-      },
-    },
-  },
-};
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, 'src')
+//       }
+//     }
+//   },
+//   css: {
+//     loaderOptions: {
+//       less: {
+//         lessOptions: {
+//           strictMath: true,
+//           noIeCompat: true,
+//         },
+//       },
+//     },
+//   },
+// };
+
